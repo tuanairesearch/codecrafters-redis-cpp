83428320f8f499444cbc48129f98b8bd8b43fe50
Bui Anh Tuan <tuan.ai.research@gmail.com>
2026-03-27 10:35:58 +0700

    codecrafters submit [skip ci]

--- src/command.cpp
+++ src/command.cpp
@@ -11,6 +11,8 @@
 #include <cstring>
 #include <sys/socket.h>
 
+// ----------- Utils for debug ---------------
+
 std::string toLowerStr(std::string s)
 {
     //std::string result = "";
@@ -22,6 +24,44 @@ std::string toLowerStr(std::string s)
     return s;
 }
 
+// ----------- Command -----------------------
+
+void handle_ping_cmd(int& client_fd) {
+    char result[] = "+PONG\r\n";
+    send(client_fd, result, strlen(result),0);
+}
+
+void handle_echo_cmd(std::vector<std::string> &inp_arr, int& client_fd) {
+    std::cout << "-----------------------" << std::endl;
+    std::cout << inp_arr.size() << std::endl;
+    std::cout << "-----------------------" << std::endl;
+    if (inp_arr.size() > 1) {
+        inp_arr[1] = handleOutput(inp_arr[1]);
+        char result[inp_arr[1].length()+1];
+        strcpy(result, inp_arr[1].c_str());
+        std::cout << "result = " << inp_arr[1] << std::endl;
+        send(client_fd, result, strlen(result),0);
+    }
+    else {
+        char result[] = "-Missing filed. Try \"echo <text>\"\r\n";
+        send(client_fd, result, strlen(result),0);
+    }
+}
+
+void handle_get_cmd() {
+
+}
+
+void handle_set_cmd() {
+
+}
+
+void handle_unknown_cmd(int& client_fd) {
+    char result[] = "-Unknown command\r\n";
+    send(client_fd, result, strlen(result),0);
+}
+
+
 void handleInput(const std::string &s, int& client_fd)
 {
     int str_pos = 0;
@@ -30,19 +70,28 @@ void handleInput(const std::string &s, int& client_fd)
     {
         //std::cout << "Check string arr" << std::endl;
         inp_arr = handleArray(s, str_pos);
-
-        if(toLowerStr(inp_arr[0]) == "echo")
+        std::string key_word = toLowerStr(inp_arr[0]);
+        if(key_word == "echo")
         {
-            inp_arr[1] = handleOutput(inp_arr[1]);
-            char result[inp_arr[1].length()+1];
-            strcpy(result, inp_arr[1].c_str());
-            std::cout << "result = " << inp_arr[1] << std::endl;
-            send(client_fd, result, strlen(result),0);
+            std::cout << "Handle echo command" << std::endl;
+            handle_echo_cmd(inp_arr, client_fd);
         }
-        else if(toLowerStr(inp_arr[0]) == "ping")
+        else if(key_word == "ping")
         {
-            char result[] = "+PONG\r\n";
-            send(client_fd, result, strlen(result),0);
+            std::cout << "Handle ping command" << std::endl;
+            handle_ping_cmd(client_fd);
+        }
+        else if (key_word == "get") {
+            std::cout << "Handle get command" << std::endl;
+            handle_get_cmd();
+        }
+        else if (key_word == "set") {
+            std::cout << "Handle set command" << std::endl;
+            handle_set_cmd();
+        }
+        else {
+            std::cout << "Handle unkown command" << std::endl;
+            handle_unknown_cmd(client_fd);
         }
         std::cout << "Checked" << std::endl;
     }
