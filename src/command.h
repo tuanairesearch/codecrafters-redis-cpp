3373b83f7100949406940bc0435f0350b66b8b54
Bui Anh Tuan <tuan.ai.research@gmail.com>
2026-04-23 14:13:29 +0700

    codecrafters submit [skip ci]

--- src/command.h
+++ src/command.h
@@ -7,10 +7,21 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <deque>
 
 std::string handleOutput(std::string &s);
 std::string toLowerStr(std::string s);
 void handleInput(const std::string &s, int& client_fd);
 std::string toLowerStr(std::string s);
-extern void send_resp_string(const char *message, int& client_fd);
+extern void send_resp_int(long long num, int& client_fd);
+extern void send_respstring(const char *message, int& client_fd);
+void send_resp_string(std::string& s, int& client_fd);
+void send_resp_string(const char *message, int& client_fd);
+void send_resp_list(std::deque<std::string>& my_list, int start_p, int end_p, int& client_fd);
+std::string handleOutput(std::string &s);
+int translate_posion(const int& position, const int& number_of_element);
+bool check_str_is_int(std::string s);
+bool check_str_is_double(std::string s);
+bool check_valid_varname(std::string& name);
+void handle_unknown_cmd(int& client_fd);
 #endif //CODECRAFTERS_REDIS_CPP_COMMAND_H
---
+++ src/list_command.h
@@ -0,0 +1,27 @@
+//
+// Created by tuan on 4/23/26.
+//
+
+#ifndef REDIS_STARTER_CPP_LIST_COMMAND_H
+#define REDIS_STARTER_CPP_LIST_COMMAND_H
+#include "list_command.h"
+#include "command.h"
+#include "global.h"
+#include <string>
+#include <iostream>
+#include <vector>
+#include <sys/socket.h>
+void handle_ping_cmd(int& client_fd);
+void handle_echo_cmd(std::vector<std::string> &inp_arr, int& client_fd);
+void handle_get_cmd(std::vector<std::string> &inp_arr, std::unordered_map<std::string, data> &client_data, int &client_fd);
+void handle_set_cmd(std::vector<std::string> &inp_arr, std::unordered_map<std::string, data> &client_data, int &client_fd);
+void handle_rpush_cmd(std::vector<std::string> &inp_arr, std::unordered_map<std::string,std::deque<std::string>> &client_data_list,int& client_fd);
+void handle_lpush_cmd(std::vector<std::string> &inp_arr, std::unordered_map<std::string,std::deque<std::string>> &client_data_list,int& client_fd);
+void handle_llen_cmd(std::vector<std::string> &inp_arr, std::unordered_map<std::string,std::deque<std::string>> &client_data_list,int& client_fd);
+void handle_lpop_cmd(std::vector<std::string> &inp_arr, std::unordered_map<std::string,std::deque<std::string>> &client_data_list,int& client_fd);
+void handle_lrange_cmd(std::vector<std::string> &inp_arr, std::unordered_map<std::string,std::deque<std::string>> &client_data_list,int& client_fd);
+void handle_blpop_cmd(std::vector<std::string> &inp_arr, std::unordered_map<std::string,std::deque<std::string>> &client_data_list,int& client_fd);
+void handle_blocked_clients(std::vector<std::string> &inp_arr,
+    std::unordered_map<std::string,std::deque<std::string>> &client_data_list,std::deque<client_time_data> &blocked_clients);
+
+#endif //REDIS_STARTER_CPP_LIST_COMMAND_H
